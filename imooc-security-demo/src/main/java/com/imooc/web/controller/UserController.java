4511a8fa0ab4ffafa4839f014751aededfd3925f
Kevin Ke <Kevin.Ke@morningstar.com>
2017-11-27 12:50:49 +0800

    Restful API

---
+++ imooc-security-demo/src/main/java/com/imooc/web/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.imooc.web.controller;
+
+import com.imooc.dto.User;
+import com.imooc.dto.UserQueryCondition;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public List<User> query(UserQueryCondition queryCondition) {
+
+        System.out.println(ReflectionToStringBuilder.toString(queryCondition, ToStringStyle.MULTI_LINE_STYLE));
+
+        List<User> users = new ArrayList<>();
+        users.add(new User());
+        users.add(new User());
+        users.add(new User());
+        return users;
+    }
+}
